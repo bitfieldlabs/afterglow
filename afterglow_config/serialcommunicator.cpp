5324e895ad8650e128338406e85be47a15726ee2
Christoph Schmid <cschmid@fastmail.net>
2018-03-07 19:45:02 +0100

    Added LGPL notice

--- afterglow_config/serialcommunicator.cpp
+++ afterglow_config/serialcommunicator.cpp
@@ -1,3 +1,26 @@
+/***********************************************************************
+ *  afterglow configuration tool:
+ *      Copyright (c) 2018 Christoph Schmid
+ *
+ ***********************************************************************
+ *  This file is part of the afterglow pinball LED project:
+ *  https://github.com/smyp/afterglow
+ *
+ *  afterglow is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  afterglow is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with afterglow.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
 #include "serialcommunicator.h"
 
 SerialCommunicator::SerialCommunicator()
